033d79670d1e60da09d88063a08eaf0c4ed4a7f1
sumit <sumitqwer1234@gmail.com>
2024-10-10 22:09:59 +0530

    design user page

--- admin/pages/courses.js
+++ admin/pages/courses.js
@@ -85,96 +85,96 @@ const courses = `
 
         </div>
     </div>
+    
+    <form class="course-form">
+    <div class="modal animate__animated animate__zoomIn" id="course-modal">
+        <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="text-xl font-semibold">Create new course</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group mb-3">
+                            <label for="thub">
+                                Thumbnail
+                            </label>
+                            <input type="file" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <select class="form-select">
+                                <option value="choose category">choose-category</option>
+                                <option value="Front-End">Front-End</option>
+                                <option value="Back-end">Back-end</option>
+                            </select>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="name">
+                                Course name
+                            </label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="link">
+                                Course link
+                            </label>
+                            <input type="url" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="price">
+                                Course Price
+                            </label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <select class="form-select">
+                                <option value="choose duration">choose-duration</option>
+                                <option value="4 month">4 month</option>
+                                <option value="6 month">6 month</option>
+                            </select>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="desc">
+                                Course description
+                            </label>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn w-full bg-blue-600 font-semibold text-white">Submit</button>
+                        <button type="button" class="d-none btn w-full bg-red-400 font-semibold">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+    <form class="course-form">
+    <div class="modal animate__animated animate__zoomIn" id="category-modal">
+        <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="text-xl font-semibold">Create new category</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group mb-3">
+                            <label for="name">
+                                Category name
+                            </label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn w-full bg-blue-600 font-semibold text-white">Submit</button>
+                        <button type="button" class="d-none btn w-full bg-red-400 font-semibold">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 
-<form class="course-form">
-<div class="modal animate__animated animate__zoomIn" id="course-modal">
-    <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="text-xl font-semibold">Create new course</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group mb-3">
-                        <label for="thub">
-                            Thumbnail
-                        </label>
-                        <input type="file" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <select class="form-select">
-                            <option value="choose category">choose-category</option>
-                            <option value="Front-End">Front-End</option>
-                            <option value="Back-end">Back-end</option>
-                        </select>
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="name">
-                            Course name
-                        </label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="link">
-                            Course link
-                        </label>
-                        <input type="url" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="price">
-                            Course Price
-                        </label>
-                        <input type="number" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <select class="form-select">
-                            <option value="choose duration">choose-duration</option>
-                            <option value="4 month">4 month</option>
-                            <option value="6 month">6 month</option>
-                        </select>
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="desc">
-                            Course description
-                        </label>
-                        <textarea class="form-control"></textarea>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn w-full bg-blue-600 font-semibold text-white">Submit</button>
-                    <button type="button" class="d-none btn w-full bg-red-400 font-semibold">Update</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
-
-
-<form class="course-form">
-<div class="modal animate__animated animate__zoomIn" id="category-modal">
-    <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="text-xl font-semibold">Create new category</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group mb-3">
-                        <label for="name">
-                            Category name
-                        </label>
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn w-full bg-blue-600 font-semibold text-white">Submit</button>
-                    <button type="button" class="d-none btn w-full bg-red-400 font-semibold">Update</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
 `;
 
 export default courses;
--- admin/pages/users.js
+++ admin/pages/users.js
@@ -1,6 +1,6 @@
 const users = `
 <div class="users">
-    <div class="grid md:grid-cols-3 gap-4">
+    <div class="animate__animated animate__zoomIn grid md:grid-cols-3 gap-4">
         <div class="p-4 bg-white shadow-sm">
             <div class="flex border-b py-3 justify-between items-center">
                 <div class="flex justify-center items-center gap-3">
@@ -75,10 +75,164 @@ const users = `
                     <h5 class="text-gray-500 font-semibold">Admin</h5>
                 </div>
             </div>
+            <div class="flex justify-between items-center">
+                <div class="border-b w-full"></div>
+                <div class="w-full flex justify-between items-center">
+                    <button class="btn bg-blue-50 text-blue-600 rounded-full">
+                        <i class="fa-regular fa-envelope"></i>
+                    </button>
+                    <div class="border-b w-full"></div>
+                    <button class="btn bg-red-50 text-red-600 rounded-full">
+                        <i class="fa-solid fa-ban"></i>
+                    </button>
+                    <button class="btn d-none bg-green-50 text-green-600 rounded-full">
+                        <i class="fa-solid fa-check"></i>
+                    </button>
+                </div>
+                <div class="border-b w-full"></div>
+            </div>
         </div>
-        <div class="p-4 bg-white shadow-sm"></div>
-        <div class="p-4 bg-white shadow-sm"></div>
     </div>
+    <button data-bs-toggle="modal" data-bs-target="#users-modal" class="position-fixed bottom-0 right-0 m-16 bg-red-500 text-white btn w-11 h-11 rounded-full">
+        <i class="fa fa-add"></i>
+    </button>
+
+    <form class="users-form">
+        <div class="modal animate__animated animate__zoomIn" id="users-modal">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="text-xl font-semibold">Create new user</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Name <sup class="text-red-500">*</sup>
+                            </label>
+                            <input type="text" name="name" required class="form-control">
+                        </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Mobile <sup class="text-red-500">*</sup>
+                            </label>
+                            <input type="number" name="mobile" required class="form-control">
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Email <sup class="text-red-500">*</sup>
+                            </label>
+                            <input type="email" name="email" required class="form-control">
+                        </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Password <sup class="text-red-500">*</sup>
+                            </label>
+                            <input type="password" name="password" required class="form-control">
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Father's name <sup class="text-red-500">*</sup>
+                            </label>
+                            <input type="text" name="father" required class="form-control">
+                        </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Qualifications <sup class="text-red-500">*</sup>
+                            </label>
+                            <select class="form-select" name="qualification">
+                                <option value="select qualification">Select Qualification</option>
+                                <option value="heigh school">Heigh School</option>
+                                <option value="inter">Inter</option>
+                                <option value="ug">UG</option>
+                                <option value="pg">PG</option>
+                            </select>
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                Status <sup class="text-red-500">*</sup>
+                            </label>
+                            <select name="status" class="form-select">
+                                <option value="select status">Select Status</option>
+                                <option value="true">Active</option>
+                                <option value="false">Pending</option>
+                            </select>
+                        </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                            <label for="name">
+                                User type <sup class="text-red-500">*</sup>
+                            </label>
+                            <select name="type" class="form-select">
+                                <option value="select user type">Select user type</option>
+                                <option value="admin">Admin</option>
+                                <option value="teacher">Teacher</option>
+                                <option value="user">user</option>
+                            </select>
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <div class="form-group">
+                            <label for="name">
+                                Select Courses <sup class="text-red-500">*</sup>
+                            </label>
+                            <select class="form-select" name="course" multiple>
+                                <option value="nodejs">Node js</option>
+                                <option value="reactjs">React js</option>
+                                <option value="php">Php</option>
+                                <option value="javascript">Javascript</option>
+                            </select>
+                        </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <div class="form-group">
+                            <label for="name">
+                                Address <sup class="text-red-500">*</sup>
+                            </label>
+                            <textarea name="address" class="form-control"></textarea>
+                        </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn w-full bg-blue-600 font-semibold text-white">Submit</button>
+                        <button type="button" class="d-none btn w-full bg-red-400 font-semibold">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 `;
 
